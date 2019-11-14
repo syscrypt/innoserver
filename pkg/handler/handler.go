ff804d597fe8f13ee5e95836723586a63facb4e6
Alex O <info@alex-olear.de>
2019-11-14 14:58:19 +0100

    feat(handler): separate route functions

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,9 +2,7 @@ package handler
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
@@ -49,41 +47,4 @@ func (s *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	router.Path("/login").Methods("GET", "POST").HandlerFunc(s.login)
 
 	router.ServeHTTP(w, r)
-}
-
-func (s *Handler) login(w http.ResponseWriter, r *http.Request) {
-	var creds model.User
-	err := json.NewDecoder(r.Body).Decode(&creds)
-	logrus.Println(creds)
-	if err != nil {
-		logrus.Errorln("login: error decoding json body", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	expirationTime := time.Now().Add(5 * time.Minute)
-
-	if _, err = s.userRepo.GetByUsername(r.Context(), creds.Name); err != nil {
-		logrus.Errorln(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	claims := &Claims{
-		Username: creds.Name,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtKey)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if _, err := w.Write([]byte(tokenString)); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 }
