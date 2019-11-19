352369427fe5ea931cc5e1f2e1bef457cb64b0cb
Alex O <info@alex-olear.de>
2019-11-19 13:54:41 +0100

    feat(authentication): authenticate user through jw-token. Pass requests user to handlers

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -19,10 +19,13 @@ type userRepository interface {
 
 type Handler struct {
 	userRepo userRepository
+
+	activeUsers map[string]*model.User
 }
 
 func NewHandler(injections ...interface{}) *Handler {
 	handler := &Handler{}
+	handler.activeUsers = make(map[string]*model.User)
 
 	for _, i := range injections {
 		switch v := i.(type) {
@@ -37,10 +40,31 @@ func NewHandler(injections ...interface{}) *Handler {
 
 func (s *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	router := mux.NewRouter()
+	r = r.WithContext(context.WithValue(r.Context(), "activeUsers", s.activeUsers))
 
 	router.Path("/login").Methods("GET").HandlerFunc(s.Login)
 	router.Path("/uploadpost").Methods("POST").HandlerFunc(s.UploadPost)
 	router.Path("/uploadpostfile").Methods("POST").HandlerFunc(s.UploadPostFile)
 
+	router.Use(authenticationMiddleware)
 	router.ServeHTTP(w, r)
+}
+
+func authenticationMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if activeUsers, ok := r.Context().Value("activeUsers").(map[string]*model.User); ok {
+			jwt := r.Header.Get("jwt")
+			if jwt != "" {
+				user := activeUsers[jwt]
+				if user != nil {
+					r = r.WithContext(context.WithValue(r.Context(), "user", user))
+					h.ServeHTTP(w, r)
+					return
+				}
+			}
+		}
+
+		w.WriteHeader(http.StatusUnauthorized)
+		h.ServeHTTP(w, r)
+	})
 }
