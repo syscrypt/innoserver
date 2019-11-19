10823411d28b5b372665e4bb4e68ec81576175c1
Alex O <info@alex-olear.de>
2019-11-19 12:02:43 +0100

    feat(database/posts): updated database schema

--- init/schema.sql
+++ init/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -6,4 +7,13 @@ CREATE TABLE users (
   email varchar(255) NOT NULL,
   imei varchar(255) NOT NULL,
   password varchar(255)
+);
+
+CREATE TABLE posts (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  user_id int,
+  path varchar(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
