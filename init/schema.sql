52e6e1ccf1574f4e6bc7a9dc9b81a65d9252c9fc
Alex O <info@alex-olear.de>
2019-12-03 11:06:44 +0100

    feat(groups): added groups and group user relationship to schema

--- init/schema.sql
+++ init/schema.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS group_user;
 DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -9,15 +11,32 @@ CREATE TABLE users (
   password varchar(255)
 );
 
+CREATE TABLE groups (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  admin_id int NOT NULL,
+  unique_id varchar(255) NOT NULL UNIQUE,
+  FOREIGN KEY(admin_id) REFERENCES users(id)
+);
+
 CREATE TABLE posts (
   id int PRIMARY KEY AUTO_INCREMENT,
   title varchar(255) NOT NULL,
-  user_id int,
+  user_id int NOT NULL,
   path varchar(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   unique_id varchar(255) NOT NULL UNIQUE,
   parent_uid varchar(255) NOT NULL,
   method int NOT NULL,
   type tinyint(1) NOT NULL,
+  group_id int DEFAULT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(group_id) REFERENCES groups(id)
+);
+
+CREATE TABLE group_user (
+  group_id int NOT NULL,
+  user_id int NOT NULL,
+  FOREIGN KEY(group_id) REFERENCES groups(id),
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
