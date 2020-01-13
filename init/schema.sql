53e6e446b360f7f475015b6dc05d8c91dd155fd5
Alex O <info@alex-olear.de>
2020-01-13 13:17:03 +0100

    feat(group/remove): updated sql schema to cascading group deletion

--- init/schema.sql
+++ init/schema.sql
@@ -41,8 +41,8 @@ CREATE TABLE posts (
 CREATE TABLE group_user (
   group_id int NOT NULL,
   user_id int NOT NULL,
-  FOREIGN KEY(group_id) REFERENCES groups(id),
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(group_id) REFERENCES groups(id) ON DELETE CASCADE,
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE options (
